2b73eacdf2879663a08c668955650512f284051e
Alex Wilson <fonzmp@gmail.com>
2018-05-08 21:34:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,7 @@
 class Dog
-  def initialize(names)
-    @name = names
-  end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
   end
 end
